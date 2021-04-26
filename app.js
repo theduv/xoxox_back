d317b3882b239a3bb3fd19bf9c849e3b288ba910
theduv <callumduval@gmail.com>
2021-04-26 03:27:52 +0200

    debugging

--- app.js
+++ app.js
@@ -184,6 +184,8 @@ io.on('connection', (socket) => {
   socket.on('changeName', (data) => {
     const room = rooms[data.room]
 
+    console.log(`${data}`)
+    console.log(room.players)
     const indexTarget = room.players.findIndex((player) => {
       return player.id === data.playerId
     })
