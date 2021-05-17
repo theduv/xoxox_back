85bfe1a6a6a419b5629b3ca9e039889e2507d1ee
theduv <tduval@student.42.fr>
2021-05-18 01:58:20 +0200

    should work for now

--- apiSide.js
+++ apiSide.js
@@ -1,15 +1,13 @@
 const express = require('express')
 const https = require('https')
 const fs = require('fs')
-const cors = require('cors')
 
 const port = process.env.PORT || 4002
 
 const app = express()
-app.use(cors())
 
-app.get('/', (req, res) => {
-  res.json({ zebi: 'test' })
+app.post('/', (req, res) => {
+  res.json({ username: req.body.username })
 })
 
 const server = https.createServer(
