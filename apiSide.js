87ae938c9874da074870fe6dd32414b59dfae76d
theduv <tduval@student.42.fr>
2021-05-18 01:01:52 +0200

    fixed ?

--- apiSide.js
+++ apiSide.js
@@ -8,8 +8,16 @@ const port = process.env.PORT || 4002
 const app = express()
 app.use(cors())
 
+const server = https.createServer(
+  {
+    key: fs.readFileSync('./back.xoxox.dev/back.xoxox.dev.key'),
+    cert: fs.readFileSync('./back.xoxox.dev/fullchain.cer'),
+  },
+  app
+)
+
 app.get('/', (req, res) => {
   res.json({ zebi: 'test' })
 })
 
-app.listen(port, () => console.log(`Listening on port 4002`))
+server.listen(port, () => console.log(`Listening on port 4002`))
